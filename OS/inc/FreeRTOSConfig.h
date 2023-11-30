913894eceee3fdf781c5c75d770bad899c69865a
Ahmed Elsayed <ahmedsliman215@gmail.com>
2023-12-01 01:15:09 +0200

    Automated-Painting-Manufactures

--- OS/inc/FreeRTOSConfig.h
+++ OS/inc/FreeRTOSConfig.h
@@ -45,7 +45,7 @@
 #define configUSE_IDLE_HOOK			0
 #define configUSE_TICK_HOOK			1
 #define configCPU_CLOCK_HZ			( ( unsigned long ) 72000000 )	
-#define configTICK_RATE_HZ			( ( TickType_t ) 2000 )
+#define configTICK_RATE_HZ			( ( TickType_t ) 500 )
 #define configMAX_PRIORITIES		( 8 )
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 150 )
 #define configTOTAL_HEAP_SIZE		( ( size_t ) ( 15 * 1024 ) )
